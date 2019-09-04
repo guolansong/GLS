73f31f57c55e5abf071539e3b9fb36ee4cbddc1d
dell <dell@DESKTOP-8SIPCH6>
2019-09-04 21:15:14 +0800

    张英政修改

--- testDemo/src/main/java/com/hq/controller/DemoController.java
+++ testDemo/src/main/java/com/hq/controller/DemoController.java
@@ -15,4 +15,12 @@ public class DemoController {
 		System.out.println("这是什么玩意");
 	}
 	
+	public void show_wc() {
+		System.out.println("傻然");
+	}
+	
+	public void show_wcsr() {
+		System.out.println("卧槽！！傻然唉");
+	}
+	
 }
